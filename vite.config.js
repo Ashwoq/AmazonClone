f5d0dc4e163096c0235b8fa0628e31b814c9be09
Ashwoq_s_ <ashwoq00786@gmail.com>
2024-03-09 16:16:20 +0530

    changes

--- vite.config.js
+++ vite.config.js
@@ -3,13 +3,13 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/AmazonClone/",
+  // base: "/AmazonClone/",
   plugins: [react()],
   server: {
     port: 3000,
     open: true,
   },
-  // build: {
-  //   outDir: "build",
-  // },
+  build: {
+    outDir: "build",
+  },
 });
